bb22b4d514c652cf33e2339e240e51d2648c4554
kabosusang <kabosusang>
2025-07-07 23:24:54 +0800

    Init Fram

---
+++ include/Macro/macro_static_reflect.hpp
@@ -0,0 +1,19 @@
+#ifndef SEREFLECT_MACRO_H_
+#define SEREFLECT_MACRO_H_
+
+// 侵入式反射示例
+#define REFLECTABLE \
+    friend class Reflection; \
+    static constexpr auto _fields = _
+
+
+
+
+
+
+
+
+
+
+
+#endif
